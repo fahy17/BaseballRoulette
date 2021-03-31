0ca2511e7ae80ccf8e3541a402b1481e09057f28
fahy17 <charlesfahy17@augustana.edu>
2021-03-30 23:35:39 -0500

    Added new activity for when the spin wheel will show up. I need to add that functionality tomorrow

--- app/src/main/java/com/example/baseballroulette/GamePage.java
+++ app/src/main/java/com/example/baseballroulette/GamePage.java
@@ -2,13 +2,31 @@ package com.example.baseballroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class GamePage extends AppCompatActivity {
+
+    Button swingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+
+        swingBtn = findViewById(R.id.swingBtn);
+
+        swingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),swingPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/baseballroulette/MainActivity.java
+++ app/src/main/java/com/example/baseballroulette/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.baseballroulette;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startBtn,settingsBtn,scoresBtn;
+    TextView titleTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        settingsBtn = findViewById(R.id.settingsBtn);
+        scoresBtn = findViewById(R.id.scoresBtn);
+        titleTxt = findViewById(R.id.titleTxt);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GamePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
